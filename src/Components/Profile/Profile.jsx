3e64b68f35e414f0753ff440dbcec01be1fadb16
Swan Yee Htet <marksleo321@gmail.com>
2025-03-20 01:01:42 +0800

    about text color change

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -14,12 +14,8 @@ function Profile() {
         <div className="col-md-6">
           <h2 className={styles.text}>Swan Yee Htet</h2>
           <p className={styles.text}>Front-end Developer</p>
-          <a href='../Swan_Yee_Htet_CV.pdf' target='_blank' rel="noopener noreferrer">
-            <button type="button" className={styles.button1}>Download CV</button>
-          </a>
-          <a href="#contact">
-            <button type='button' className={styles.button2}>Contact Info</button>
-          </a><br />
+          <a href='../Swan Yee Htet_CV Form.pdf' target='_blank'><button type="button" className={styles.button1}>Download CV</button></a>
+          <button type='button' className={styles.button2}>Contact Info</button><br />
           <section className={`mt-2 mb-5`}>
             <a href='https://www.linkedin.com/in/swan-yee-htet-65804b33a/'><FaLinkedin className={styles.social} /></a>
             <a href='https://github.com/Mark-S-Leo'><FaGithubSquare className={styles.social} /></a>
